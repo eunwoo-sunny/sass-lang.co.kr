8b0e00bf114446b2f673587163aff0b40ab7376a
ejey <eunwoo-sunny@gmail.com>
2023-02-08 03:37:53 +0900

    Header,Footer create

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <Footer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/CommonHeader";
+import Footer from "./components/CommonFooter";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Footer,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/CommonFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer>
+    <small
+      >It is available for use and modification under the MIT License.</small
+    >
+  </footer>
+</template>
